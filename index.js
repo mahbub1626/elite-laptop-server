9d29529ef2f44165fdb2208b4b64b5b3ebb25253
mahbub1626 <mahbub1626@gmail.com>
2022-11-25 17:54:21 +0600

    jwt token worked

--- index.js
+++ index.js
@@ -1,13 +1,98 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
 require('dotenv').config();
+const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3sh2wxl.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// jwt verify 
+function verifyJWT(req, res, next) {
+    console.log('token inside VerifyJWT', req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+
+    })
+
+}
+
+
+async function run() {
+    try {
+        const laptopsCollection = client.db('eliteLaptop').collection('laptops');
+        const usersCollection = client.db('eliteLaptop').collection('users');
+
+
+
+        // laptop categories
+        app.get('/categories', async (req, res) => {
+            const email = req.query.email;
+            console.log('token', req.headers.authorization);
+            const decodedEmail = req.decoded.email;
+            console.log(email, decodedEmail)
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const query = { email: email };
+            const bookings = await laptopsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        })
+
+        // get users 
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        })
+
+        // create users
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', async (req, res) => {
     res.send('Elite Laptop portal server is running')
