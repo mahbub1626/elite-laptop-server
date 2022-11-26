a58e74d92acc0f461732f396978b6eaccc56cf9a
mahbub1626 <mahbub1626@gmail.com>
2022-11-26 23:01:49 +0600

    users and category get separately

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 
@@ -39,25 +39,19 @@ function verifyJWT(req, res, next) {
 
 async function run() {
     try {
+        const laptopsCategoriesCollection = client.db('eliteLaptop').collection('categories');
         const laptopsCollection = client.db('eliteLaptop').collection('laptops');
         const usersCollection = client.db('eliteLaptop').collection('users');
 
 
 
-        // laptop categories
+        // laptop categories    
         app.get('/categories', async (req, res) => {
-            const email = req.query.email;
-            console.log('token', req.headers.authorization);
-            const decodedEmail = req.decoded.email;
-            console.log(email, decodedEmail)
-            if (email !== decodedEmail) {
-                return res.status(403).send({ message: 'forbidden access' })
-            }
-
-            const query = { email: email };
-            const bookings = await laptopsCollection.find(query).toArray();
-            res.send(bookings);
+            const query = {};
+            const categories = await laptopsCategoriesCollection.find(query).toArray();
+            res.send(categories);
         })
+
 
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
@@ -68,6 +62,15 @@ async function run() {
                 return res.send({ accessToken: token });
             }
             res.status(403).send({ accessToken: '' })
+        })
+
+
+        // get buyer
+        app.get('/users/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({ isBuyer: user?.userType === 'buyer' });
         })
 
         // get users 
@@ -85,6 +88,36 @@ async function run() {
             res.send(result);
         });
 
+        // post a laptop
+        app.post('/laptops', verifyJWT, async (req, res) => {
+            const laptop = req.body;
+            const result = await laptopsCollection.insertOne(laptop);
+            res.send(result);
+        })
+
+        app.get('/laptops', async (req, res) => {
+            const query = {};
+            const laptops = await laptopsCollection.find(query).toArray();
+            res.send(laptops);
+        })
+        // get uniq category by _id
+        app.get('/laptops/:id', async (req, res) => {
+            const id = req.params.id;
+            // const query = {categoryId:}
+            const query = { _id: ObjectId(id) }
+            console.log(id, query);
+
+            const categoryLaptop = await laptopsCollection.findOne(query)
+            res.send(categoryLaptop);
+        })
+
+        // get uniq category by categoryId
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { categoryId: id }
+            const categoryLaptop = await laptopsCollection.find(query).toArray()
+            res.send(categoryLaptop);
+        })
 
     }
     finally {
